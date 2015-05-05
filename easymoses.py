aece898af24d3973e67e36c5b70119d92f7defa7
RainAndSnow <545135082@qq.com>
2015-05-05 09:04:51 +0800

    lion100-0

--- easymoses.py
+++ easymoses.py
@@ -48,7 +48,6 @@ def limiting_sentence_length (cfg_info) :
 	print "finish 1"
 
 
-#def language_model_training (cfg_info) :
 	
 def corpus_preparation (cfg_info) :
 	#tokenisation (cfg_info)
@@ -57,7 +56,44 @@ def corpus_preparation (cfg_info) :
 	limiting_sentence_length (cfg_info)
 
 
+def generate_sb (cfg_info) :
+	print "generate .sb. "
+	os.system (cfg_info.irstlm_path + "bin/add-start-end.sh < " \
+		+ cfg_info.training_path + "../" + cfg_info.filename + ".true." + cfg_info.target_id + " > "\
+		+ cfg_info.lm_path + cfg_info.filename + ".sb." + cfg_info.target_id)
+
+def generate_lm (cfg_info) :
+	print "generate lm"
+	os.system ("export IRSTLM=" + cfg_info.irstlm_path + "; " + cfg_info.irstlm_path + "bin/build-lm.sh " \
+		+ "-i " + cfg_info.lm_path + cfg_info.filename + ".sb." + cfg_info.target_id + " -t ./tmp -p -s " \
+		+ "improved-kneser-ney -o " + cfg_info.lm_path + cfg_info.filename + ".lm." + cfg_info.target_id)
+
+def generate_arpa (cfg_info) :
+	print "generate arpa"
+	os.system (cfg_info.irstlm_path + "bin/compile-lm --text=yes " \
+		+ cfg_info.lm_path + cfg_info.filename + ".lm." + cfg_info.target_id + ".gz " \
+		+ cfg_info.lm_path + cfg_info.filename + ".arpa." + cfg_info.target_id)
+
+def generate_blm (cfg_info) :
+	print "generate blm"
+	os.system (cfg_info.mosesdecoder_path + "bin/build_binary " \
+		+ cfg_info.lm_path + cfg_info.filename + ".arpa." + cfg_info.target_id + " " \
+		+ cfg_info.lm_path + cfg_info.filename + ".blm." + cfg_info.target_id)
+
+def language_model_training (cfg_info) :
+	print "language model training"
+	generate_sb (cfg_info)
+	generate_lm (cfg_info) 
+	generate_arpa (cfg_info)
+	generate_blm (cfg_info) 
+	print "finsih language model training"
+
+
+
+
+
 def easymoses ():
 	cfg_info = ConfigInfo.ConfigInfo("config")
-	corpus_preparation (cfg_info)
+	#corpus_preparation (cfg_info)
+	language_model_training (cfg_info)
 
