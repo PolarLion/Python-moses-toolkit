59338d9d965476efa1f36b029adff1e8311b86b1
RainAndSnow <545135082@qq.com>
2015-05-10 13:19:44 +0800

    lion100-1

--- easynlpm.py
+++ easynlpm.py
@@ -1,17 +1,18 @@
 import os
 import sys
 import re
+import time
 
 
 
 class CfgInfo :
 	nplm = "/opt/translation/nplm/"
 	corpus_path = "~/corpora/nplmt/training/"
-	filename = "cht.train.zh-cn.txt"
+	filename = "cht.train.en"
 	ngram_size = "5"
-	vocab_size = "50000"
+	vocab_size = "100000"
 	validation_size = "500"
-	num_epochs = "10"
+	num_epochs = "20"
 	num_hidden = "0"
 	input_embedding_dimension = "150"
 	output_embedding_dimension = "750"
@@ -24,13 +25,14 @@ cfg = CfgInfo ()
 ######################### Training NPLM #############################
 
 def prepare_neural_language_model (cfg_info) :
-	os.system (cfg_info.nplm + "bin/prepareNeuralLM --train_text " + cfg_info.corpus_path + cfg_info.filename + " " \
-		+ " --ngram_size " + cfg_info.ngram_size + " " + "--vocab_size " + cfg_info.vocab_size + \
+	os.system (cfg_info.nplm + "bin/prepareNeuralLM " \
+		+ " --train_text " + cfg_info.corpus_path + cfg_info.filename \
+		+ " --ngram_size " + cfg_info.ngram_size + " " + "--vocab_size " + cfg_info.vocab_size \
 		+ " --write_words_file " + cfg_info.corpus_path + "../words " \
 		+ " --train_file " + cfg_info.corpus_path + "../train.ngrams " \
 		+ " --validation_size " + cfg_info.validation_size \
 		+ " --validation_file " + cfg_info.corpus_path + "../validation.ngrams " \
-		+ " >& " +cfg_info.corpus_path + "../prepareout.out &")
+		+ " >& " + cfg_info.corpus_path + "../prepareout.out &")
 
 def train_neural_network (cfg_info) :
 	os.system (cfg_info.nplm + "bin/trainNeuralNetwork " \
@@ -48,5 +50,11 @@ def train_neural_network (cfg_info) :
 
 
 
+start = time.clock ()
 #prepare_neural_language_model (cfg)
+end = time.clock ()
+#print "prepare_neural_language_model used ", (start - end) / 1000000, "s"
+start = time.clock ()
 train_neural_network (cfg)
+end = time.clock ()
+#print "train_neural_network used ", (start - end) / 1000000, "s"
